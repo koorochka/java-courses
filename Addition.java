1097f3e4c4b2788e86abcefa594d72c789540360
Артём Курочка < >
2016-04-02 00:08:05 +0300

    Create Addition sample

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Additin sample");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int result = first + second;
+		System.out.println("The result is = " + result);
+	}
+
+}
