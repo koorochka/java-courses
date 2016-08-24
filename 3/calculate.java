d5cbc0b98cbd6508c7d3afe99800432ec2f5d93c
Koorochka <artemij@koorochka.ru>
2016-08-24 19:26:38 +0300

    calculator + Integer.valueOf System.out.println

--- 3/calculate.java
+++ 3/calculate.java
@@ -1,7 +1,10 @@
 
 public class calculate{
-	public static void main(String[] args)
+	public static void main(String[] arg)
 	{
-		System.out.println("Calculator blank");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		System.out.println("sum: " + summ);
 	}
 }
