f08e132d4494006a4dffa9788142c53ec05e581f
Koorochka <artemij@koorochka.ru>
2016-08-24 20:07:53 +0300

    add to repo calculator.java

--- 4/calculate.java
+++
@@ -1,9 +0,0 @@
-public class calculate{
-	public static void main(String[] arg)
-	{
-		int first = Integer.valueOf(arg[0]);
-		int second = Integer.valueOf(arg[1]);
-		int summ = first + second;
-		System.out.println("sum: " + summ);
-	}
-}
---
+++ 4/calculator.java
@@ -0,0 +1,36 @@
+/**
+	Класс реализует калькулятор
+  */
+public class calculator{
+	/**
+		Результат вычисления
+	*/
+	private int result;
+
+	/**
+		Суммируем аргументы
+		@param params Аргументы суммирования
+	*/
+	public void add(int ... params)
+	{
+		for(Integer param : params){
+			this.result += param;
+		}
+	}
+
+	/**
+		Получить результат
+		@return результат вычисления
+	*/
+	public int getResult(){
+		return this.result;
+	}
+
+	/**
+		Очистить результат вычисления
+	*/
+	public void clearResult()
+	{
+		this.result = 0;
+	}
+}
